4c88e405bae0beaf7ca3c1dc94fc39d33f3ff308
Nicholas Westerhausen <nwesterhausen@gmail.com>
2018-12-04 09:53:16 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+generateRadios();
+
+function generateRadios() {
+    let contentdiv = $("#content");
+    for (let special of Object.keys(PERKS)) {
+        contentdiv.append("<legend>" + special + "</legend>");
+        for (let i = 0; i < PERKS[special].length; i++) {
+            let radios = "<div class='row'><span class='col-sm-3'>"+PERKS[special][i].name+"</span>" +
+                "<div class=\"btn-group btn-group-toggle col-sm-3\" data-toggle=\"buttons\">";
+            let groupname = PERKS[special][i].name.replace(/ /g, "-").toLowerCase();
+            for (let j = 1; j <= PERKS[special][i].ranks; j++)
+                radios += "<label class=\"btn btn-outline-primary\">" +
+                    "<input type=\"radio\" name=\""+groupname+"\" id=\"rank"+j+"\" autocomplete=\"off\"> Rank" + j +
+                    "</label>";
+            radios += "</div></div>";
+            contentdiv.append(radios);
+        }
+    }
+}
