475d5ca7f9c631fb2c0125b2c43d3191c0797077
Nicholas Westerhausen <nwesterhausen@gmail.com>
2018-12-04 14:27:18 -0500

    Added save functions.

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ $("input").change(function () {
 });
 // Register menu buttons
 $("#btnClearAll").click(clearAllData);
+$("#btnCopyTo").click(copyToClipboard);
+$("#btnDownload").click(download);
 
 function generateRadios() {
     let contentdiv = $("#content");
@@ -81,4 +83,42 @@ function clearAllData() {
         $("#"+alertid).remove();
     }, 5000);
 
+}
+
+function copyToClipboard() {
+    saveState();
+    const textArea = document.createElement('textarea');
+    textArea.id = "tempTA";
+    textArea.textContent = state;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    $('#tempTA').remove();
+    let alertid = Date.now();
+    $("#alertbox").append("<div id='"+alertid+"' class=\"alert alert-success\">" +
+        "Copied current state to clipboard!" +
+        "</div>");
+    setTimeout(function() {
+        $("#"+alertid).remove();
+    }, 5000);
+}
+
+/**
+ * Modified from the example here:
+ * https://stackoverflow.com/a/18197341
+ */
+function download() {
+    let filename = 'perks-helper.txt';
+    let text = state;
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/yaml;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
