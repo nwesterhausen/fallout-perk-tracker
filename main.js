b6c122b0588da0b58e5db3501024933247df6c6a
Nicholas Westerhausen <nwesterhausen@gmail.com>
2018-12-04 09:59:35 -0500

    Dark theme

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function generateRadios() {
                 "<div class=\"btn-group btn-group-toggle col-sm-3\" data-toggle=\"buttons\">";
             let groupname = PERKS[special][i].name.replace(/ /g, "-").toLowerCase();
             for (let j = 1; j <= PERKS[special][i].ranks; j++)
-                radios += "<label class=\"btn btn-outline-primary\">" +
+                radios += "<label class=\"btn btn-outline-light\">" +
                     "<input type=\"radio\" name=\""+groupname+"\" id=\"rank"+j+"\" autocomplete=\"off\"> Rank" + j +
                     "</label>";
             radios += "</div></div>";
